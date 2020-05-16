283d56e3f90ec2046950d4c188fb08fec6578681
Andrey Rogozhnikov <rogozhnikov.andrey@gmail.com>
2020-05-16 23:28:26 +0300

    Sixth

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import os, datetime
 
-
-from settings import *
-import solarhouse.export as ex
+import settings
 from solarhouse.calculation import Calculation
+import solarhouse.export as export
 
 
 def main():
     calc = Calculation(
-                        TZ, 
-                        PATH_FILE_OBJECT,
-                        GEO, 
-                        WALL_MATERIAL,
-                        WALL_THICKNESS, 
-                        TEMPERATURE_START, 
-                        POWER_HEAT_INSIDE,
-                        EFF,
+                        settings.TZ, 
+                        settings.GEO, 
+                        settings.PATH_FILE_OBJECT,
+                        settings.WALL_MATERIAL,
+                        settings.WALL_THICKNESS, 
+                        settings.TEMPERATURE_START, 
+                        settings.POWER_HEAT_INSIDE,
+                        settings.EFF,
                         heat_accumulator={'volume': 0.02, 'material': 'water'},
                         windows={'area': 0.3, 'therm_r': 5.0},
                         floor={'area': 1.0, 'material': 'adobe', 'thickness': 0.2, 't_out': 4.0},
                     )
     date = datetime.datetime(day=22, month=7, year=2019)
     calc_id = calc.id
-    if not os.path.exists(os.path.join(PATH_OUTPUT, calc_id)):
-        os.mkdir(os.path.join(PATH_OUTPUT, calc_id))
+    if not os.path.exists(os.path.join(settings.PATH_OUTPUT, calc_id)):
+        os.mkdir(os.path.join(settings.PATH_OUTPUT, calc_id))
     data_frame = calc.compute(date=22, month=12, year=2019, with_weather=False)
-    ex.as_file(data_frame, 'csv', os.path.join(PATH_OUTPUT, calc_id))
-    ex.as_html(data_frame, os.path.join(PATH_OUTPUT, calc_id))
+    export.as_file(data_frame, 'csv', os.path.join(settings.PATH_OUTPUT, calc_id))
+    export.as_html(data_frame, os.path.join(settings.PATH_OUTPUT, calc_id))
    
-    with open(os.path.join(PATH_OUTPUT, calc_id, 'data.csv'), 'r') as file:
+    with open(os.path.join(settings.PATH_OUTPUT, calc_id, 'data.csv'), 'r') as file:
         print(file.read())
 
 
-main()
+if __name__ == "__main__":
+    main()
